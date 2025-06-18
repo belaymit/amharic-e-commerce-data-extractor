04ca5632b7123d569fc52a0a1c21ecdb26579b50
belaymit <2belamit@gmail.com>
2025-06-18 17:05:38 +0300

    featture: Add modular source code structure

---
+++ src/pipeline/__init__.py
@@ -0,0 +1,5 @@
+"""Pipeline module for the Amharic E-commerce Data Extractor."""
+
+from .data_ingestion import DataIngestionPipeline, run_task_1
+
+__all__ = ["DataIngestionPipeline", "run_task_1"] 
